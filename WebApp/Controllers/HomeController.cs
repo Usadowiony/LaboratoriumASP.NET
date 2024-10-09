c3330655cfb880a4211e5d9170f3b42b50257abe
Usadowiony <wojciechpietrzak2003@gmail.com>
2024-10-09 11:47:39 +0200

    make functional calculator by query

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -25,6 +25,35 @@ public class HomeController : Controller
     
     public IActionResult Calculator()
     {
+        //https://localhost:7101/Home/Calculator?op=add&x=4&y=1,5
+        var op = Request.Query["op"];
+        var x = double.Parse(Request.Query["x"]);
+        var y = double.Parse(Request.Query["y"]);
+        var result = 0.0d;
+
+        switch (op)
+        {
+            case "add":
+                result = x + y;
+                ViewBag.Operator = "+";
+                break;
+            case "sub":
+                result = x - y;
+                ViewBag.Operator = "-";
+                break;
+            case "mul":
+                result = x * y;
+                ViewBag.Operator = "*";
+                break;
+            case "div":
+                result = x / y;
+                ViewBag.Operator = ":";
+                break;
+        }
+
+        ViewBag.Result = result;
+        ViewBag.X = x;
+        ViewBag.Y = y;
         return View();
     }
 
