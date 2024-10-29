06f3dc5b2076df0c9ed97f67df40ee35b1d51e92
Usadowiony <wojciechpietrzak2003@gmail.com>
2024-10-29 18:09:59 +0100

    commit

--- WebApp/Controllers/CalculatorController.cs
+++ WebApp/Controllers/CalculatorController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
@@ -14,45 +15,15 @@ namespace WebApp.Controllers
             return View();
         }
 
-        public IActionResult Result(Operator? op, double? x, double? y)
+        public IActionResult Result(Calculator model)
         {
-            if (x is null || y is null)
+            if (!model.IsValid())
             {
-                ViewBag.ErrorMessage = "Niepoprawny format liczby x lub y lub ich brak!";
-                return View("CalculatorError");
+                return View("Error");
             }
-
-            if (op is null)
-            {
-                ViewBag.ErrorMessage = "Nieznany operator!";
-                return View("CalculatorError");
-            }
-            double? result = 0.0d;
-
-            switch (op)
-            {
-                case Operator.Add:
-                    result = x + y;
-                    ViewBag.Operator = "+";
-                    break;
-                case Operator.Sub:
-                    result = x - y;
-                    ViewBag.Operator = "-";
-                    break;
-                case Operator.Mul:
-                    result = x * y;
-                    ViewBag.Operator = "*";
-                    break;
-                case Operator.Div:
-                    result = x / y;
-                    ViewBag.Operator = ":";
-                    break;
-            }
-
-            ViewBag.Result = result;
-            ViewBag.X = x;
-            ViewBag.Y = y;
-            return View();
+            return View(model);
         }
+
+        
     }
 }
