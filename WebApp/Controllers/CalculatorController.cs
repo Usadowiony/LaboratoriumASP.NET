e2115af3e331b14eadbad0fbded6ed2aaba59841
Usadowiony <wojciechpietrzak2003@gmail.com>
2024-10-29 17:37:49 +0100

    new branch lab2 and few changes to project

---
+++ WebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        public IActionResult Result(Operator? op, double? x, double? y)
+        {
+            if (x is null || y is null)
+            {
+                ViewBag.ErrorMessage = "Niepoprawny format liczby x lub y lub ich brak!";
+                return View("CalculatorError");
+            }
+
+            if (op is null)
+            {
+                ViewBag.ErrorMessage = "Nieznany operator!";
+                return View("CalculatorError");
+            }
+            double? result = 0.0d;
+
+            switch (op)
+            {
+                case Operator.Add:
+                    result = x + y;
+                    ViewBag.Operator = "+";
+                    break;
+                case Operator.Sub:
+                    result = x - y;
+                    ViewBag.Operator = "-";
+                    break;
+                case Operator.Mul:
+                    result = x * y;
+                    ViewBag.Operator = "*";
+                    break;
+                case Operator.Div:
+                    result = x / y;
+                    ViewBag.Operator = ":";
+                    break;
+            }
+
+            ViewBag.Result = result;
+            ViewBag.X = x;
+            ViewBag.Y = y;
+            return View();
+        }
+    }
+}
