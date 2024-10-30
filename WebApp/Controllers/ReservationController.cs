aeaa9ae0ab96dd2cf91876f280b01086daad2024
Usadowiony <wojciechpietrzak2003@gmail.com>
2024-10-30 10:39:04 +0100

    make lab3

---
+++ WebApp/Controllers/ReservationController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ReservationController : Controller
+{
+    static Dictionary<int, Reservation> _reservations = new Dictionary<int, Reservation>();
+
+    public IActionResult Index()
+    {
+        return View(_reservations);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Reservation model)
+    {
+        if (ModelState.IsValid)
+        {
+            int id = _reservations.Keys.Count != 0 ? _reservations.Keys.Max() : 0;
+            model.Id = id + 1;
+            _reservations.Add(model.Id, model);
+
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            return View(model);
+        }
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        if (_reservations.ContainsKey(id))
+        {
+            return View(_reservations[id]);
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Reservation model)
+    {
+        if (ModelState.IsValid)
+        {
+            _reservations[model.Id] = model;
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            return View(model);
+        }
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        if (_reservations.ContainsKey(id))
+        {
+            return View(_reservations[id]);
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        if (_reservations.ContainsKey(id))
+        {
+            _reservations.Remove(id);
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpGet]
+    public IActionResult Details(int id)
+    {
+        if (_reservations.ContainsKey(id))
+        {
+            return View(_reservations[id]);
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+}
---
+++ WebApp/Models/Reservation.cs
@@ -0,0 +1,32 @@
+﻿namespace WebApp.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class Reservation
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Proszę podać datę!")]
+        public DateTime Data { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać miasto!")]
+        public string Miasto { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać adres!")]
+        public string Adres { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać pokój!")]
+        public string Pokój { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać właściciela!")]
+        public string Właściciel { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Proszę podać poprawną cenę!")]
+        public double Cena { get; set; }
+    }
+
+}
